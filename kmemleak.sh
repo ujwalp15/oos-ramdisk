672b8d8d0651065a182bc07a4bbb33b891309532
ujwalp15 <ujwalp09@gmail.com>
2017-11-04 23:19:35 +0530

    oos-ramdisk: OOS Open-Beta O

--- kmemleak.sh
+++ kmemleak.sh
@@ -9,6 +9,10 @@ if [ -f /sys/kernel/debug/kmemleak ]; then
         cat /sys/kernel/debug/kmemleak > /sdcard/oem_log/kmemleak.txt
         echo `date` >> /sdcard/oem_log/ps.txt
         ps >> /sdcard/oem_log/ps.txt
+        echo `date` >> /sdcard/oem_log/ion_system.txt
+        cat /sys/kernel/debug/ion/heaps/system >> /sdcard/oem_log/ion_system.txt
+        echo `date` >> /sdcard/oem_log/ion_qsecom.txt
+        cat /sys/kernel/debug/ion/heaps/qsecom >> /sdcard/oem_log/ion_qsecom.txt
 
         sleep 3600
     done
